039eaaf31893a69a86b53dc51d5ce6836debfe28
rosh <rosh@pop-os.localdomain>
2020-05-24 22:38:55 +0530

    Basic build successfull

--- winstall.sh
+++ winstall.sh
@@ -5,15 +5,15 @@ if [[ $1 == "make" ]];then
 			echo "make sure you entered the thing correctly"
 			exit
 		fi
-	echo "yea"
+	echo "Making $2..."
 	mkdir "$2.win"
 	cd "$2.win"
 	apt-get download $2 && apt-cache depends -i $2 | awk '/Depends:/ {print $2}' | xargs  apt-get download
-	if [ !$(ls -A ) ];then
+	if [ ! "$(ls -A )" ];then
 		cd ..
 		rm -r "$2.win"
 	fi
-	echo "$2.win"
+	echo "Done making $2.win."
 fi
 
 if [[ $1 == "install" ]];then
@@ -21,7 +21,7 @@ if [[ $1 == "install" ]];then
 		then echo "Please run as root"
 		exit
 	fi
-	echo "Oh yeah"
+	echo "Installing.."
 	dpkg -i *.deb
 fi
 
